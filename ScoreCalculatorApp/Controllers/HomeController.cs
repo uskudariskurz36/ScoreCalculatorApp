8451546be885e7325448bd4d6e2373b5e9c82dc4
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-15 10:59:17 +0300

    Add project files.

---
+++ ScoreCalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScoreCalculatorApp.Models;
+using System.Diagnostics;
+
+namespace ScoreCalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(/*new IndexViewModel()*/);
+        }
+
+        [HttpPost]
+        public IActionResult Index(IndexViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Percent1 + model.Percent2 != 100)
+                {
+                    ModelState.AddModelError(nameof(model.Percent1), "Percent 1 ve Percent 2 toplamı 100 olmalıdır.");
+                    ModelState.AddModelError(nameof(model.Percent2), "Percent 1 ve Percent 2 toplamı 100 olmalıdır.");
+                }
+                else
+                {
+                    model.CalculatedScore = (model.Score1 * model.Percent1 / 100) + (model.Score2 * model.Percent2 / 100);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ScoreCalculatorApp/Models/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoreCalculatorApp.Models
+{
+    public class IndexViewModel
+    {
+        [Range(0, 100)]
+        public int Score1 { get; set; }
+
+        [Range(0, 100)]
+        public int Score2 { get; set; }
+
+        [Range(0,100)]
+        public int Percent1 { get; set; }
+
+        [Range(0, 100)]
+        public int Percent2 { get; set; }
+
+        public int CalculatedScore { get; set; }
+    }
+}
